624fb281255e895a984e79aeb5dfabe77e6b892e
MDueP <mdupedersen@outlook.dk>
2024-02-09 12:24:56 +0100

    virker med wifi og ekstern client

--- DHT11/dht11.py
+++ DHT11/dht11.py
@@ -2,10 +2,14 @@ import Adafruit_DHT
 import sqlite3
 import datetime
 from time import sleep
+from socket import *
 sensor = Adafruit_DHT.DHT11
 pin = 4
 HUMIDITY, TEMPERATURE = Adafruit_DHT.read_retry(sensor, pin)
 query = """INSERT INTO DHT11 (DATETIME, TEMPERATURE, HUMIDITY) VALUES(?,?,?)"""
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+modtagerport = 12000
+modtageraddr = ""
 while HUMIDITY is not None and TEMPERATURE is not None:
     #Vigtigt den er inde i loopet, ellers bliver tiden ikke opdateret:
     data = (datetime.datetime.now(), TEMPERATURE, HUMIDITY)
@@ -14,9 +18,20 @@ while HUMIDITY is not None and TEMPERATURE is not None:
         cur = conn.cursor()
         cur.execute(query, data)
         conn.commit()
+    except sqlite3.Error as e:
+        conn.rollback()
+        print(f'Could not insert! {e}')
     finally:
         cur.execute("""SELECT * FROM DHT11 ORDER BY ID DESC LIMIT 20""") #limit fortæller at det er de første 20, med order by ID DESC, vender tabellen og de nyeste commits er nu i toppen
         dataudtræk=cur.fetchall()
         print(dataudtræk) #Det er ikke kønt, men det virker
+        try:
+            cur.execute("""SELECT * FROM DHT11 ORDER BY ID DESC LIMIT 1""")
+            data_client=cur.fetchall()
+            strdata_client = str(data_client)
+            serverSocket.sendto(strdata_client.encode(), (modtageraddr, modtagerport))
+        except Exception as error:
+            print(f"Wifi Error '{error}', no connection")
+            pass
         conn.close
         sleep(10)
